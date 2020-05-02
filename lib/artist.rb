e0d2565836dccce6984dbf43441492699f92a767
Claudia Pierson <c172@umbc.edu>
2020-05-02 03:55:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,16 +14,16 @@ class Artist
     song.artist = self 
   end 
  
-  def songs 
-    Song.all.select {|song| song.artist = self}
-  end 
+  # def songs 
+  #   Song.all.select {|song| song.artist = self}
+  # end 
 
-  def self.find_or_create_by_name
-  end 
+  # def self.find_or_create_by_name
+  # end 
 
-  def print_songs
-    self.songs.each{|song| puts song.name}
-  end 
+  # def print_songs
+  #   self.songs.each{|song| puts song.name}
+  # end 
 
   
   end 
