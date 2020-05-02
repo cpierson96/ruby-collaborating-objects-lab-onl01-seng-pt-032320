99d60fd2b703394ba42eb65b75f71262da915a25
Claudia Pierson <c172@umbc.edu>
2020-05-02 03:37:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,8 @@ class Artist
    @@all 
   end 
 
-  def self.find_or_create_by_name
-  end 
+  # def self.find_or_create_by_name
+  # end 
 
   # def print_songs
   #   self.songs.each{|song| puts song.name}
